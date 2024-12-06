73ffbdcfd614991f93ba441213627a1285037869
yoann tourtellier <tourtellier.yoann@gmail.com>
2024-12-06 18:45:08 +0100

    Initial Commit

---
+++ MotivationDuJour/MotivationDuJourApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotivationDuJourApp.swift
+//  MotivationDuJour
+//
+//  Created by Yoann TOURTELLIER on 06/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MotivationDuJourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MotivationDuJourTests/MotivationDuJourTests.swift
@@ -0,0 +1,17 @@
+//
+//  MotivationDuJourTests.swift
+//  MotivationDuJourTests
+//
+//  Created by Yoann TOURTELLIER on 06/12/2024.
+//
+
+import Testing
+@testable import MotivationDuJour
+
+struct MotivationDuJourTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
